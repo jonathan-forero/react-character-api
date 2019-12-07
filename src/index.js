4ccb8913e30876a00101464aad39ef5eb740896e
jonathan.forero <jonathan-forero@globant.com>
2019-12-07 15:14:44 -0500

    React project oilerplate

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const container = document.getElementById('root');
+
+ReactDOM.render(<div>HOLA MUNDO</div> , container);
