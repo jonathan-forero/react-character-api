1c38d55a8edc87834d4b3b4b7492f4cdab3f1a1f
jonathan.forero <jonathan-forero@globant.com>
2019-12-07 17:57:32 -0500

    build files

---
+++ build/precache-manifest.a140bafee9a1da25a660ba6352f776b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076f5c5476b7333f33fe52662d500a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "235f3919c6164f049d99",
+    "url": "/static/css/main.05524053.chunk.css"
+  },
+  {
+    "revision": "2557fd1abba35f061897",
+    "url": "/static/js/2.45354edb.chunk.js"
+  },
+  {
+    "revision": "235f3919c6164f049d99",
+    "url": "/static/js/main.aac1b8fc.chunk.js"
+  },
+  {
+    "revision": "86d2e57775a4d69124bd",
+    "url": "/static/js/runtime-main.91b834ab.js"
+  },
+  {
+    "revision": "e3b9a208e084bce49eebe9e0b8e66a0a",
+    "url": "/static/media/breaking_bad.e3b9a208.png"
+  }
+]);
