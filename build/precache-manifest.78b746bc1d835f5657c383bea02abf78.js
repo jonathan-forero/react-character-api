2fc03c75bbbf050de4c93904a3f050e31c750795
jonathan.forero <jonathan-forero@globant.com>
2019-12-11 00:31:11 -0500

    rebuilding site

---
+++ build/precache-manifest.78b746bc1d835f5657c383bea02abf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce12e777724c13dcbe9138280782b17c",
+    "url": "/react-character-api/index.html"
+  },
+  {
+    "revision": "235f3919c6164f049d99",
+    "url": "/react-character-api/static/css/main.05524053.chunk.css"
+  },
+  {
+    "revision": "2557fd1abba35f061897",
+    "url": "/react-character-api/static/js/2.45354edb.chunk.js"
+  },
+  {
+    "revision": "235f3919c6164f049d99",
+    "url": "/react-character-api/static/js/main.aac1b8fc.chunk.js"
+  },
+  {
+    "revision": "05b2d711eb2dcd44ab74",
+    "url": "/react-character-api/static/js/runtime-main.57cd6128.js"
+  },
+  {
+    "revision": "e3b9a208e084bce49eebe9e0b8e66a0a",
+    "url": "/react-character-api/static/media/breaking_bad.e3b9a208.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a140bafee9a1da25a660ba6352f776b6.js"
+  "/react-character-api/precache-manifest.78b746bc1d835f5657c383bea02abf78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-character-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
